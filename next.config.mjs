a162c771d3700b7c398317fb314bc8c2ead64639
Hamza705-tech <hamzacontact247@gmail.com>
2024-12-11 05:39:54 +0530

    third commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     eslint: {
-        ignoreDuringBuilds: true,
+      ignoreDuringBuilds: true, // Use if ESLint errors are blocking deployment
     },
-};
-
-module.exports = nextConfig;
+    // Add more configuration options
+    poweredByHeader: false, // Remove the `X-Powered-By` header
+    compress: true, // Enable gzip compression
+    i18n: {
+      locales: ['en'],
+      defaultLocale: 'en',
+    },
+    images: {
+      domains: ['example.com'], // Add domains for next/image
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin',
+            },
+          ],
+        },
+      ];
+    },
+  }; 
+  export default nextConfig;
