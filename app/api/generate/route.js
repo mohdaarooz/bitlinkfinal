39d2fa6c4f71d2c9781316e2164e35840954eab0
Hamza705-tech <hamzacontact247@gmail.com>
2024-12-11 07:35:10 +0530

    six

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,15 +1,39 @@
-import clientPromise from "@/lib/mongodb";
+// import clientPromise from "@/lib/mongodb";
+
+// export async function POST(request) {
+//   try {
+//     const client = await clientPromise;
+//     const db = client.db("bitlinks");
+//     // ... बाकी का कोड यहाँ ...
+//   } catch (error) {
+//     console.error("Database connection error:", error);
+//     return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+//       status: 500,
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+//   }
+// }
+
+
+import clientPromise from "@/lib/mongodb"
 
 export async function POST(request) {
-  try {
+
+    const body = await request.json() 
     const client = await clientPromise;
-    const db = client.db("bitlinks");
-    // ... बाकी का कोड यहाँ ...
-  } catch (error) {
-    console.error("Database connection error:", error);
-    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
+    const db = client.db("bitlinks")
+    const collection = db.collection("url")
+
+    // Check if the short url exists
+    const doc = await collection.findOne({shorturl: body.shorturl})
+    if(doc){
+        return Response.json({success: false, error: true,  message: 'URL already exists!' })
+    }
+
+    const result = await collection.insertOne({
+        url: body.url,
+        shorturl: body.shorturl
+    })
+
+    return Response.json({success: true, error: false,  message: 'URL Generated Successfully' })
   }
-}
