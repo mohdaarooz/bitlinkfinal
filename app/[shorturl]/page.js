49a060827778c34b1535cb60d97f46353329847e
Hamza705-tech <hamzacontact247@gmail.com>
2024-12-11 07:27:59 +0530

    fifth

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -1,22 +1,23 @@
 import { redirect } from "next/navigation"
 import clientPromise from "@/lib/mongodb"
-
-
 export default async function Page({ params }) {
-    const shorturl = (await params).shorturl
+    try {
+        const shorturl = params.shorturl
 
-    const client = await clientPromise;
-    const db = client.db("bitlinks")
-    const collection = db.collection("url")
+        const client = await clientPromise;
+        const db = client.db("bitlinks")
+        const collection = db.collection("url")
 
-    const doc = await collection.findOne({shorturl: shorturl})
-    console.log(doc)
-    if(doc){
-         redirect(doc.url)
+        const doc = await collection.findOne({shorturl: shorturl})
+        console.log(doc)
+        if(doc){
+             return redirect(doc.url)
+        }
+        else{
+            return redirect(`${process.env.NEXT_PUBLIC_HOST}`)
+        }
+    } catch (error) {
+        console.error("Error in [shorturl] page:", error);
+        return redirect(`${process.env.NEXT_PUBLIC_HOST}/error`)
     }
-    else{
-        redirect(`${process.env.NEXT_PUBLIC_HOST}`)
-    }
-
-    return <div>My Post: {url}</div>
-  }
+}
