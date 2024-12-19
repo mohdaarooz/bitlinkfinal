1b74c11af90e28213e5a94738b46ce3bdc328062
mohdaarooz <168467760+mohdaarooz@users.noreply.github.com>
2024-12-19 14:55:58 +0530

    Update page.js

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -1,22 +1,20 @@
-import { redirect } from "next/navigation"
-import clientPromise from "@/lib/mongodb"
-
+import { redirect } from "next/navigation";
+import clientPromise from "@/lib/mongodb";
 
 export default async function Page({ params }) {
-    const shorturl = (await params).shorturl
+    const { shorturl } = params;
+
+    if (!shorturl || typeof shorturl !== "string") {
+        return redirect(process.env.NEXT_PUBLIC_HOST || "/");
+    }
 
     const client = await clientPromise;
-    const db = client.db("bitlinks")
-    const collection = db.collection("url")
+    const db = client.db("bitlinks");
+    const collection = db.collection("url");
 
-    const doc = await collection.findOne({shorturl: shorturl})
-    console.log(doc)
-    if(doc){
-         redirect(doc.url)
+    const doc = await collection.findOne({ shorturl });
+    if (doc && doc.url) {
+        return redirect(doc.url);
     }
-    else{
-        redirect(`${process.env.NEXT_PUBLIC_HOST}`)
-    }
-
-    return <div>My Post: {url}</div>
-  }
+    return redirect(process.env.NEXT_PUBLIC_HOST || "/");
+}
